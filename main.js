8f0d5ad6bfd6fac3ba783dcb561c744e1a2a943f
pudders <ericpudney@gmail.com>
2023-12-07 16:12:48 +0100

    adjusted storage function, styling

--- main.js
+++ main.js
@@ -1,36 +1,22 @@
+// DOM elements
 const canvas = document.getElementById("canvas");
 const score = document.getElementById("score");
 const highScores = document.getElementById("hi-scores");
 
-const renderScoreList = () => {
-  let scoresArray = [];
-  for (var i = 0; i < localStorage.length; i++) {
-    const value = localStorage.getItem(localStorage.key(i));
-    const key = localStorage.key(i);
-    if (isNaN(value)) {
-      continue;
-    } else {
-      scoresArray.push({ key: key, value: value });
-    }
-  }
-  scoresArray.sort((a, b) => b.value - a.value);
-
-  for (let i = 0; i < scoresArray.length; i++) {
-    let currentScore = document.createElement("h1");
-    currentScore.innerText = scoresArray[i].key + ": " + scoresArray[i].value;
-    highScores.appendChild(currentScore);
-  }
-};
-
-highScores.addEventListener("load", renderScoreList());
-
+// defines canvas
 const ctx = canvas.getContext("2d");
 canvas.width = 500;
-canvas.height = 700;
+canvas.height = 600;
 
-let bgImg = new Image();
+// global variables
+let playing = false;
+const obstacles = [];
+let points = 0;
+let lastTime = Date.now();
+const spawnDelay = 4;
+let spawnTimer = 0;
+const bgImg = new Image();
 bgImg.src = "./images/background.png";
-
 let player = {
   width: 50,
   height: 40,
@@ -40,13 +26,39 @@ let player = {
   movement: 0,
 };
 
-let playing = false;
-let obstacles = [];
-let points = 0;
-let lastTime = Date.now();
+// generates high score list from local storage
+const renderScoreList = () => {
+  let scores = localStorage.getItem("hi-scores");
+  let scoresArray = scores ? JSON.parse(scores) : [];
+  scoresArray.sort((a, b) => b.score - a.score);
 
-let spawnTimer = 200;
+  let table = document.createElement("table");
+  let header = document.createElement("tr");
+  header.innerHTML = "<th colspan='2'>High Scores</th>";
+  let header2 = document.createElement("tr");
+  header2.innerHTML = "<th>Player</th><th>Score</th>";
+  table.append(header, header2);
+  for (let i = 0; i < scoresArray.length; i++) {
+    let playerPoints = scoresArray[i];
 
+    let row = document.createElement("tr");
+    let name = document.createElement("td");
+    let points = document.createElement("td");
+    points.style.textAlign = "right";
+
+    name.innerText = playerPoints.name;
+    points.innerText = playerPoints.score;
+
+    row.append(name, points);
+    table.append(row);
+  }
+  highScores.append(table);
+};
+
+// loads high scores when page is loaded
+highScores.addEventListener("load", renderScoreList());
+
+// tick function
 const tick = () => {
   ctx.drawImage(bgImg, 0, 0, canvas.width, canvas.height);
 
@@ -60,15 +72,16 @@ const tick = () => {
     obstacles[i].x -= 1;
   }
 
-  score.innerText = points;
+  score.innerText = "Current player score: " + points;
 
   if (spawnTimer <= 0) {
     generateObstacles();
-    spawnTimer = 200;
+    spawnTimer = spawnDelay;
   } else {
-    spawnTimer--; //use deltatime here
+    spawnTimer -= deltaTime;
   }
 
+  // moves player
   if (player.jump) {
     player.movement = 0;
     player.movement -= 2;
@@ -77,23 +90,28 @@ const tick = () => {
   }
   player.y += player.movement;
 
+  // checks for defeat
+  if (
+    player.y - player.height > canvas.height ||
+    player.y + player.height < 0
+  ) {
+    endGame();
+  }
   for (let obstacle of obstacles) {
-    if (
-      registerCollision(player, obstacle) ||
-      player.y - player.height > canvas.height ||
-      player.y + player.height < 0
-    ) {
+    if (registerCollision(player, obstacle)) {
       endGame();
     }
   }
 
+  // if no defeat, calls tick function again
   if (playing) {
     requestAnimationFrame(tick);
   }
 };
 
-restartGame();
+startGame();
 
+// ends game, clears screen, updates high scores
 const endGame = () => {
   playing = false;
   ctx.drawImage(bgImg, 0, 0, canvas.width, canvas.height);
@@ -104,16 +122,27 @@ const endGame = () => {
     canvas.height / 2 - 10
   );
   let playerName = prompt("Whats your name?");
-  localStorage.setItem(playerName, points);
+  saveScore(playerName, points);
   highScores.innerHTML = "";
   renderScoreList();
+  // score.innerText = "";
 };
 
-function restartGame() {
-  const allScores = { ...localStorage };
-  console.log(allScores);
+// Saves the latest score to local storage.
+function saveScore(name, score) {
+  let scoresArray = localStorage.getItem("hi-scores");
+  scoresArray = scoresArray ? JSON.parse(scoresArray) : [];
+  scoresArray.push({
+    name,
+    score,
+  });
+  localStorage.setItem("hi-scores", JSON.stringify(scoresArray));
+}
+
+// starts or restarts the game
+function startGame() {
   ctx.drawImage(bgImg, 0, 0, canvas.width, canvas.height);
-  obstacles = [];
+  obstacles.length = 0;
   points = 0;
   player = {
     width: 40,
@@ -127,6 +156,7 @@ function restartGame() {
   requestAnimationFrame(tick);
 }
 
+// draws the flappy bird
 const drawPlayer = (playerObj) => {
   let playerImg = new Image();
   playerImg.src = "./images/bird.png";
@@ -139,6 +169,7 @@ const drawPlayer = (playerObj) => {
   );
 };
 
+// draws the walls
 const drawObstacles = (obstacles) => {
   for (let i = 0; i < obstacles.length; i++) {
     let currentObstacle = obstacles[i];
@@ -156,17 +187,19 @@ const drawObstacles = (obstacles) => {
   }
 };
 
+// generate walls with randomly-placed gaps in an array
 const generateObstacles = () => {
   let yPos = Math.floor(Math.random() * canvas.height);
   let xPos = canvas.width;
-  yPos < 275 ? (yPos = 275) : (yPos = yPos);
-  yPos > canvas.height - 200 ? (yPos = canvas.height - 200) : (yPos = yPos);
+  yPos < 200 ? (yPos = 200) : (yPos = yPos);
+  yPos > canvas.height - 125 ? (yPos = canvas.height - 125) : (yPos = yPos);
   obstacles.push(
     { x: xPos, y: 0, width: 70, height: yPos - 120 },
     { x: xPos, y: yPos, width: 70, height: canvas.height - yPos }
   );
 };
 
+// checks for collisions
 const registerCollision = (rect1, rect2) => {
   if (
     rect1.x < rect2.x + rect2.width &&
@@ -180,16 +213,18 @@ const registerCollision = (rect1, rect2) => {
   }
 };
 
+// enables player to control movement
 window.addEventListener("keydown", function (event) {
   if (event.key === " ") {
     if (playing) {
       player.jump = true;
     } else {
-      restartGame();
+      startGame();
     }
   }
 });
 
+// causes bird to fall if not rising
 window.addEventListener("keyup", function (event) {
   if (event.key === " ") {
     player.jump = false;
